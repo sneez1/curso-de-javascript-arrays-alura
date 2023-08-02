7d4da06cb2db4d42723d19701495e315f7829318
sneez1 <hugoeirmao@gmail.com>
2023-08-02 14:00:04 -0300

    curso completo de arrays da alura

---
+++ callbacks.js
@@ -0,0 +1,14 @@
+const nomes = ['Evaldo', 'Mari', 'Camis'];
+
+//nomes.forEach(function (nome){
+//})
+//
+//nomes.forEach( (nome) => {
+//    console.log(nome)
+//})
+
+function imprimeNome(nome) {
+    console.log(nome);
+}
+
+nomes.forEach(imprimeNome)
